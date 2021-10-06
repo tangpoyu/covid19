6b2ccd143bf46ab46340c21f87fe6e3970886730
cena <b10623041@gemail.yuntech.edu.tw>
2021-10-07 00:52:14 +0800

    two

---
+++ src/components/LineChart.vue
@@ -0,0 +1,45 @@
+// 參考以前的文章可能產生版本不支持問題
+
+<script>
+import { Line } from 'vue-chartjs'
+
+export default {
+  extends: Line,
+
+  props: {
+    label: {
+      type: String,
+    },
+    chartData: {
+      type: Array,
+    },
+    options: {
+      type: Object,
+    },
+    chartColorOptions: {
+      type: Object,
+    },
+  },
+
+  mounted() {
+    const dates=this.chartData.map((d) => d.date)
+    const totals=this.chartData.map((d) => d.suffer)
+    console.log(dates)
+    console.log(totals)
+    this.renderChart(
+      {
+        labels:dates,
+        datasets: [
+          {
+            label: this.label,
+            data: totals,
+            ...this.chartColorOptions
+          },
+        ],
+      },
+      this.options
+    )
+  },
+}
+</script>
+
---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/covid19/'
+  : '/',
+
+  transpileDependencies: [
+    'vuetify'
+  ]
+}
