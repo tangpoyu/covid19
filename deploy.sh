6b4e008d87bf3df7649b5515f2a3bcf15672d981
cena <b10623041@gemail.yuntech.edu.tw>
2021-10-07 01:18:50 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,15 @@
+//deploy.sh
 #!/usr/bin/env sh
-# abort on errors
+# 當發生錯誤時終止腳本運行
 set -e
-# build
+# 打包
 npm run build
-# navigate into the build output directory
+# 移動至到打包後的dist目錄 
 cd dist
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-git init
+git init //因為dist資料夾預設是被ignore的，因此在進入dist資料夾後初始化git
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:tangpoyu/covid19.git master:gh-pages
+# 部署到 https://github.com/chou0728/eric-project.git 分支為 gh-pages
+git push -f https://github.com/tangpoyu/covid19.git master:gh-pages
+//將dist資料夾中的內容推送至遠端eric-project的gh-pages分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
 cd -
