fbc94d82aa17bcc34c1b5f5c195abcc82392801f
cena <b10623041@gemail.yuntech.edu.tw>
2021-10-07 01:23:08 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,6 @@ git commit -m 'deploy'
 # 部署到 https://github.com/chou0728/eric-project.git 分支為 gh-pages
 git push -f https://github.com/tangpoyu/covid19.git master:gh-pages
 //將dist資料夾中的內容推送至遠端eric-project的gh-pages分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
-cmd /k dir
+
 cd -
-cmd /k dir
+exec /bin/bash
